84b9e12a84e3d637d722293c669d576f0f32725a
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-18 06:33:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -11,6 +11,6 @@ class Project
   def add_backer(backer)
     @backers << backer
     backer.back_project(self) unless backer.backed_projects.include?(self)
-     binding.pry
+    # binding.pry
   end
 end
